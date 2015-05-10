46a497744bf1a167f1224163695c91b38ea7ac45
Anthony Sottile <asottile@umich.edu>
2015-05-10 09:41:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+[![Build Status](https://travis-ci.org/asottile/pip-custom-platform.svg?branch=master)](https://travis-ci.org/asottile/pip-custom-platform)
+[![Coverage Status](https://img.shields.io/coveralls/asottile/pip-custom-platform.svg?branch=master)](https://coveralls.io/r/asottile/pip-custom-platform)
+
+pip-custom-platform
+===================
+
+pip+wheel wrapper which allows you to choose a custom platform name for
+building, downloading, and installing wheels.
+
+pip: https://github.com/pypa/pip
+wheel: https://bitbucket.org/pypa/wheel
+
+## Installation
+
+TODO
+
+## Usage
+
+TODO
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pip_custom_platform',
+    description=(
+        'pip + wheel wrapper which allows you to choose a custom platform '
+        'name for building, downloading, and installing wheels.'
+    ),
+    url='https://github.com/asottile/pip-custom-platform',
+    version='0.0.0',
+    author='Anthony Sottile',
+    author_email='asottile@umich.edu',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages('.', exclude=('tests*', 'testing*')),
+    install_requires=[
+        'argparse',
+        'pip',
+        'wheel',
+    ],
+    entry_points={
+        'console_scripts': [],
+    },
+)
