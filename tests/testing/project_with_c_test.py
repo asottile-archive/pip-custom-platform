df4a27a6438080d6b5fd1ecc5b4cb12f397201a1
Anthony Sottile <asottile@umich.edu>
2015-05-10 11:08:30 -0700

    Add a C extension package for testing

---
+++ tests/testing/project_with_c_test.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import subprocess
+
+
+def test_project_with_c(tmpdir):
+    """Make sure it is an installable C extension and produces the expected
+    output.
+    """
+    venv = tmpdir.join('venv')
+    subprocess.check_call(('virtualenv', venv.strpath))
+    subprocess.check_call((
+        'sh', '-c',
+        '. {0}/bin/activate && pip install testing/project_with_c'.format(
+            venv.strpath,
+        ),
+    ))
+    proc = subprocess.Popen(
+        (
+            'sh', '-c',
+            '. {0}/bin/activate && '
+            'python -c "'
+            'import project_with_c\n'
+            'print(project_with_c.hello_world())"'.format(
+                venv.strpath,
+            )
+        ),
+        stdout=subprocess.PIPE,
+    )
+    out, _ = proc.communicate()
+    assert proc.returncode == 0
+    assert out == b'hello world\n'
