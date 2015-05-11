254ce5f9e6398dcb785066a6ba1f3eebb1d2c232
Anthony Sottile <asottile@umich.edu>
2015-05-10 20:58:58 -0700

    Fix pypy a bit

--- testing/util.py
+++ testing/util.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import sys
+
 from distlib.wheel import ABI
 from distlib.wheel import IMPVER
 
 
+def is_py2_or_pypy():
+    return str is not bytes or '__pypy__' in sys.builtin_module_names
+
+
 def expected_wheel_name(fmt):
-    return fmt.format(IMPVER, 'none' if str is bytes else ABI)
+    return fmt.format(IMPVER, 'none' if is_py2_or_pypy() else ABI)
