952a9f0ae2dc4fdb7e801f358506108746616d7c
Alexander Regueiro <alexreg@me.com>
2017-12-15 19:02:49 +0000

    Fixed for Python 3.6 / pip 9.0.1 on Mac OS X 10.13.
    Added monkey patch for pep425tags modules.

--- pip_custom_platform/pymonkey.py
+++ pip_custom_platform/pymonkey.py
@@ -11,6 +11,11 @@ def pymonkey_patch(mod, args):
     if mod.__name__ == 'distutils.util':
         from pip_custom_platform.default_platform import get_platform_func
         mod.get_platform = get_platform_func(args, mod.get_platform)
+    elif mod.__name__ in ('pip.pep425tags', 'pip._internal.pep425tags'):
+        from pip_custom_platform.default_platform import get_platform_func
+        mod.get_platform = get_platform_func(args, mod.get_platform)
+        mod.supported_tags = mod.get_supported()
+        mod.supported_tags_noarch = mod.get_supported(noarch=True)
     elif mod.__name__ in ('pip', 'pip._internal') and hasattr(mod, 'main'):
         from pip_custom_platform._main import get_main
         mod.main = get_main(mod.main)
