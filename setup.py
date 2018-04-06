44fa9aaadfb05ba2b0ddefbdf9451b4869374d4a
Anthony Sottile <asottile@umich.edu>
2018-04-06 15:42:13 -0700

    v0.4.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         'name for building, downloading, and installing wheels.'
     ),
     url='https://github.com/asottile/pip-custom-platform',
-    version='0.4.0',
+    version='0.4.1',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
