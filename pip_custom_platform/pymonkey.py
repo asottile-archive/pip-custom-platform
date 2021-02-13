7091fe5d79d1abfe89e83788cf16b1c95c6d9e88
Aaron Loo <admin@aaronloo.com>
2021-02-12 17:02:38 -0800

    documenting pymonkey_patch

--- pip_custom_platform/pymonkey.py
+++ pip_custom_platform/pymonkey.py
@@ -7,15 +7,28 @@ def pymonkey_argparse(argv):
     return parser.parse_known_args(argv)
 
 
-def pymonkey_patch(mod, args):
-    if mod.__name__ == 'distutils.util':
+def pymonkey_patch(module, args):
+    """
+    This is where the magic happens:
+        1. Monkey-patch `pip`'s `main` function, so that we can inject our own
+           custom parser. This allows us to provide additional functionality
+           (like `show-platform-name`) on top of the standard `pip` interface.
+
+        2. Monkey-patch `pip`'s default functionality to identify the platform
+           that the user is running on, and substitute it with our own (better)
+           platform parser.
+    """
+    if module.__name__ == 'distutils.util':
         from pip_custom_platform.default_platform import get_platform_func
-        mod.get_platform = get_platform_func(args, mod.get_platform)
-    elif mod.__name__ in ('pip.pep425tags', 'pip._internal.pep425tags'):
+        module.get_platform = get_platform_func(args, module.get_platform)
+    elif module.__name__ in ('pip.pep425tags', 'pip._internal.pep425tags'):
         from pip_custom_platform.default_platform import get_platform_func
-        mod.get_platform = get_platform_func(args, mod.get_platform)
-        mod.supported_tags = mod.get_supported()
-        mod.supported_tags_noarch = mod.get_supported(noarch=True)
-    elif mod.__name__ in ('pip', 'pip._internal') and hasattr(mod, 'main'):
+        module.get_platform = get_platform_func(args, module.get_platform)
+        module.supported_tags = module.get_supported()
+        module.supported_tags_noarch = module.get_supported(noarch=True)
+    elif (
+        module.__name__ in ('pip', 'pip._internal') and
+        hasattr(module, 'main')
+    ):
         from pip_custom_platform._main import get_main
-        mod.main = get_main(mod.main)
+        module.main = get_main(module.main)
